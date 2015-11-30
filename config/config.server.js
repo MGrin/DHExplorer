6fdd45c2342da56405661a87311a0ac4389e75d8
Nikita Grishin <mr6r1n@gmail.com>
2015-11-30 16:35:24 +0100

    New server setup

--- config/config.server.js
+++ config/config.server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var ENV = process.env.NODE_ENV || 'development';
+var PORT = process.env.PORT || 7890;
 var path = require('path');
 var rootPath = path.normalize(__dirname + '/..');
 
@@ -13,16 +14,16 @@ var config = {
   },
 
   production: {
-    port: 7890,
+    port: PORT,
     root: rootPath,
-    serverUrl: 'http://128.178.21.39:7890/',
+    serverUrl: 'http://dhexplorer.org/',
     name: 'Garzoni Data Explorer'
   }
 };
 
 config = config[ENV];
 config.ENV = ENV;
-config.default_sparql_endpoint = (ENV === 'production') ? 'http://128.178.21.39:8890' : 'http://192.168.99.100:8890/sparql';
-config.default_graph_name = (ENV === 'production') ? 'http://128.178.21.39:8080/garzoni-data' : 'http://localhost:8080/garzoni-data';
+config.default_sparql_endpoint = (ENV !== 'development') ? 'http://128.178.21.22:8890/sparql' : 'http://192.168.99.100:8890/sparql';
+config.default_graph_name = (ENV !== 'development') ? 'http://128.178.21.22:8080/garzoni-data' : 'http://localhost:8080/garzoni-data';
 
 module.exports = config;
--- public/js/services/config.service.js
+++ public/js/services/config.service.js
@@ -17,8 +17,8 @@
     entity: {
       perPage: 30,
     },
-    default_sparql_endpoint: (app.env === 'production') ? 'http://128.178.21.39:8890' : 'http://localhost:8890/sparql',
-    default_graph_name: (app.env === 'production') ? 'http://128.178.21.39:8080/garzoni-data/' : 'http://localhost:8080/garzoni-data/',
+    default_sparql_endpoint: (app.env === 'production') ? 'http://128.178.21.22:8890/sparql' : 'http://localhost:8890/sparql',
+    default_graph_name: (app.env === 'production') ? 'http://128.178.21.22:8080/garzoni-data/' : 'http://localhost:8080/garzoni-data/',
     default_query: 'select ?person ?contract where {?person a grz-owl:PersonMention . ?contract a grz-owl:Contract}'
   };
 
